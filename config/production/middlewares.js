2c36177a4a0f5d1f77a183e82d83fc46b583239e
Daniel Laurindo <dslaurindo@gmail.com>
2022-08-25 07:05:18 -0300

    Adding valid origin to prod middleware

---
+++ config/production/middlewares.js
@@ -0,0 +1,47 @@
+module.exports = [
+  "strapi::errors",
+  "strapi::poweredBy",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "https://criptoapp-blog-admin.herokuapp.com",
+        "https://criptoapp.com.br",
+      ],
+    },
+  },
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "res.cloudinary.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+];
